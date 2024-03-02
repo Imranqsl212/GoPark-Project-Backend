af63eba118058ae6ea461503aed43e7ad0123390
EMMMABK <adilbikiev2@gmail.com>
2024-03-02 12:39:09 +0600

    add Filter fix

--- ride/views.py
+++ ride/views.py
@@ -22,19 +22,26 @@ def create_ride(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def get_all_rides(request):
-    rides = Ride.objects.annotate(num_applicants=Count("applications")).filter(
-        num_applicants__lt=F("max_applicants")
-    )
+    start_time = request.query_params.get('start_time', None)
+    max_applicants = request.query_params.get('max_applicants', None)
+
+    if start_time:
+        rides = Ride.objects.filter(start_time__gte=start_time)
+
+    if max_applicants:
+        rides = Ride.objects.filter(max_applicants__gte=max_applicants)
+
+    if not start_time and not max_applicants:
+        rides = Ride.objects.all()
+
     paginator = CustomRidesPagination()
     result_page = paginator.paginate_queryset(rides, request)
     serializer = RideSerializer(result_page, many=True)
 
     return paginator.get_paginated_response(serializer.data)
-
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
@@ -104,23 +111,4 @@ def delete_ride(request, ride_id):
     ride.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def get_all_rides(request):
-    start_time = request.query_params.get('start_time', None)
-    max_applicants = request.query_params.get('max_applicants', None)
 
-    if start_time:
-        rides = Ride.objects.filter(start_time__gte=start_time)
-
-    if max_applicants:
-        rides = Ride.objects.filter(max_applicants__gte=max_applicants)
-
-    if not start_time and not max_applicants:
-        rides = Ride.objects.all()
-
-    paginator = CustomRidesPagination()
-    result_page = paginator.paginate_queryset(rides, request)
-    serializer = RideSerializer(result_page, many=True)
-
-    return paginator.get_paginated_response(serializer.data)
